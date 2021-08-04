3104df03407576d595d62e96d8b0e9dd8d266233
momoladebrouill <momoladebrouill@gmail.com>
2021-08-04 11:13:54 +0200

    Update main.js

--- web version/main.js
+++ web version/main.js
@@ -40,21 +40,22 @@ function scroll(cb){
 	nbsy=parseInt(H/SIZE)
 }
 function click(e) {
-	switch(e.button){
-		case 0:
+	switch(e.buttons){
+		case 1:
 			mode='c'
 			break;
-		case 1:
+		case 2:
+			mode='take'
 			sel=lieux[[x,y]]
 			break;
-		case 2:
+		case 4:
 			mode='e'
 			break;
 	}
 }
 function keydown(e){
 	v=e.keyCode
-
+	console.log(v)
 	switch(v){
 		case 81:
 			depx+=10
@@ -78,6 +79,14 @@ function keydown(e){
 			depx=0
 			depy=0
 			break;
+		case 67:
+			sel=lieux[[x,y]]
+			break;
+		case 16:
+			devmode= !devmode
+			break;
+		case 32:
+			helpmode= !helpmode
 	}
 	sel=sel%1.2
 	document.body.style.backgroundColor = coul(sel)
@@ -100,7 +109,8 @@ let lieux={}
 let mode=''
 let sel=1
 let depx=0,depy=0;
-let devmode=false
+let devmode=true;
+let helpmode=true;
 let x=10
 let y=10
 let i,j;
@@ -108,27 +118,34 @@ let SIZE =75
 let fakeSIZE=75
 let nbsx=W/SIZE
 let nbsy=H/SIZE
-context.font = "50px cursive";
+context.font = "25px consolas";
 context.textBaseline = "middle"
 
 function bLoop() {
-	context.beginPath()
-	context.fillStyle=coul(sel)
-	context.fillRect(0,0,W,H)
-	context.fill()
-	context.closePath()
-
-	context.beginPath()
-	context.fillStyle='black'
-	context.fillRect(5,5,W-10,H-10)
-	context.fill()
-	context.closePath()
+	if(devmode){
+		context.beginPath()
+		context.fillStyle=coul(sel)
+		context.fillRect(0,0,W,H)
+		context.fill()
+		context.closePath()
+		context.beginPath()
+		context.fillStyle='black'
+		context.fillRect(5,5,W-10,H-10)
+		context.fill()
+		context.closePath()
+	}else{
+		context.beginPath()
+		context.fillStyle='black'
+		context.fillRect(0,0,W,H)
+		context.fill()
+		context.closePath()
+	}
 
 	dep.x+=(depx-dep.x)/7
 	dep.y+=(depy-dep.y)/7
 	fakeSIZE+=(SIZE-fakeSIZE)/7
 	if(mode=='c'){
-		lieux[[x,y]]=coul(sel)
+		lieux[[x,y]]=sel
 	}else if(mode=='e'){
 		if(find(x,y)){
 			lieux[[x,y]]=undefined
@@ -140,16 +157,24 @@ function bLoop() {
 		j=parseInt(pos[1])+0.5
 		if(i>-depx/SIZE-1 && j>-depy/SIZE-2 && i<-depx/SIZE+nbsx+1 &&j<-depy/SIZE+nbsy+1 ){
 			context.beginPath()
-			context.fillStyle=lieux[clef]
+			context.fillStyle=coul(lieux[clef])
 			context.fillRect(i*SIZE+dep.x,j*SIZE+dep.y,SIZE,SIZE)
 			context.fill()
 			context.closePath()
 		}
 	}
-	context.beginPath()
-	context.fillStyle = "white";
-	context.fillText(mode,0,100)
-	context.closePath()
+	if(helpmode){
+		context.fillStyle="black"
+		context.fillRect(0,0,W/2,7*25)
+		context.fillStyle = "white";
+		let decal=25
+		let ls=['Maj -> masquer la bordure','Flèches -> se déplacer','Molette -> zoomer','W/X -> changer de couleur','Echap -> recentrer',"Espace -> cacher cette inutilité"]
+		for(elem in ls ){
+			context.fillText(ls[elem],0,decal);
+			decal+=25
+		}
+
+	}
 	requestAnimationFrame(bLoop)
 }
 bLoop()
